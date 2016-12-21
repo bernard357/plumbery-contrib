0a461814461350af048774ae538850c895fee81c
Bernard Paques <bernard.paques@gmail.com>
2016-12-21 11:49:42 +0100

    bump 1.0.4 -- first version aligned with release of plumbery

---
+++ HISTORY.md
@@ -0,0 +1,5 @@
+# History
+
+## 1.0.4 (2016-12-21)
+
+* First release aligned with plumbery
--- __init__.py
+++ __init__.py
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 __all__ = ['__version__']
-__version__ = '0.1'
+__version__ = '1.0.4'
